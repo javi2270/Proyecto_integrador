cea1f05a54f403557c75feeef02cce062c6bcf3a
Javier Villafañe <carlosjavier.villafane@estudiantes.unahur.edu.ar>
2025-09-05 07:19:04 -0300

    add middleware auth

---
+++ src/middlewares/auth.validator.js
@@ -0,0 +1,21 @@
+const jsonwebtoken = require('jsonwebtoken')
+const { Rol, Usuario} = require('../models/index')
+
+const validarToken = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const esAdministrador = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = { validarToken, esAdministrador }
+
