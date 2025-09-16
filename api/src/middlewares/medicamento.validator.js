f7e8f8b3c64ad346744af4a075658264072ece13
Javier Villafañe <carlosjavier.villafane@estudiantes.unahur.edu.ar>
2025-09-16 19:10:58 -0300

    add docs BRD FRD

--- api/src/middlewares/medicamento.validator.js
+++ api/src/middlewares/medicamento.validator.js
@@ -3,7 +3,7 @@ const { crearMedicamentoSchema, actualizarMedicamentoSchema } = require('../sche
 const validarMedicamento = (req, res, next) => {
     const { error } = crearMedicamentoSchema.validate(req.body)
     if (error) {
-        return res.status(400).jason({ 
+        return res.status(400).json({ 
             error: 'Error de validacion', 
             mensaje: error.details[0].message
         })
@@ -14,7 +14,7 @@ const validarMedicamento = (req, res, next) => {
 const validarActualizacionMedicamento = (req, res, next) => {
     const { error } = actualizarMedicamentoSchema.validate(req.body)
     if (error) {
-        return res.status(400).jason({
+        return res.status(400).json({
             error: 'Error al actualizar',
             mensaje: error.details[0].message
         })
