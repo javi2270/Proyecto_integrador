80880a1e84b13f22d399b18ef62f200f9bf86cb4
Javier Villafañe <carlosjavier.villafane@estudiantes.unahur.edu.ar>
2025-09-22 19:45:32 -0300

    corrigiendo archivo .env

--- api/src/services/temperatura.service.js
+++ api/src/services/temperatura.service.js
@@ -1,2 +1,20 @@
 const Temperatura = require('../models/temperatura.model')
 
+const temperaturaService = {}
+
+const registrarTemperatura = async (datos) => {
+    // extraigo los datos necesarios
+    const { valor, usuarioId } = datos
+
+    // creo una nueva instancia del modelo temperatura
+    const nuevoRegistro = new Temperatura({
+        valor: valor,
+        usuario: usuarioId
+    })
+
+    // guardo la instancia en la base de datos y la retorno
+    return await nuevoRegistro.save()
+}
+temperaturaService.registrarTemperatura = registrarTemperatura
+
+module.exports = temperaturaService
